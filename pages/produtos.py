8411589454cd8134396363af224b44e089c20430
Fornelos <thiagofornelos@gmail.com>
2025-08-26 09:02:10 -0300

    Exclusão  pages produtos e uniades

--- pages/produtos.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-import plotly.express as px
-import streamlit as st
-
-st.set_page_config(layout='wide',page_title="Produtos",page_icon="📈")
--- pages/unidades.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-import plotly.express as px
-import streamlit as st
-
-st.set_page_config(layout='wide',page_title="Unidades",page_icon="📊")
